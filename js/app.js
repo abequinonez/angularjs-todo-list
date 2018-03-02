0f020513b93876c94c352da57b74f397adf270ba
abequinonez <abequinonez@users.noreply.github.com>
2018-03-02 15:31:27 -0800

    feat: Add item-adding functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,31 @@
 const app = angular.module('todoListApp', []);
 app.controller('TodoListCtrl', function() {
+  this.inputValue = '';
+  this.listItems = [];
 
+  // Add a new item to the list of items
+  this.addItem = function(evt) {
+    /*
+    For readability, declare the following constant variables containing the
+    numerical values of both the target key pressed by the user and the enter
+    key.
+    */
+    const targetKey = evt.which;
+    const enterKey = 13;
+
+    /*
+    Also declare a constant that contains the result of checking whether or not
+    the input contains a non-whitespace character.
+    */
+    const containsNonWhitespaceChar = this.inputValue.match(/\S/);
+
+    /*
+    Before adding an item, check if the key values match and if the input
+    contains a non-whitespace character.
+    */
+    if (targetKey === enterKey && containsNonWhitespaceChar) {
+      this.listItems.push(this.inputValue);
+      this.inputValue = '';
+    }
+  };
 });
